6455ce4f9d5fa92d811ba9f35e86a7094b947a37
Maksim <i@mpozdnjakov.ru>
2024-04-22 11:46:31 +0300

    22th commit

--- app/src/main/java/project/moms/assistant/presentation/fragments/FragmentAssistant.kt
+++ app/src/main/java/project/moms/assistant/presentation/fragments/FragmentAssistant.kt
@@ -1,19 +1,28 @@
 package project.moms.assistant.presentation.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import project.moms.assistant.R
-
+import androidx.fragment.app.Fragment
+import project.moms.assistant.databinding.FragmentAssistantBinding
 
 class FragmentAssistant : Fragment() {
+    private var _binding : FragmentAssistantBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_assistant, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = FragmentAssistantBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
