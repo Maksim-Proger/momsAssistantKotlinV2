61ad6cd7efaa4fae9c2477e7bab9ef751554e4b6
Maksim <i@mpozdnjakov.ru>
2024-04-22 11:29:14 +0300

    21th commit

---
+++ app/src/main/java/project/moms/assistant/presentation/fragments/SleepConfiguratorFragment.kt
@@ -0,0 +1,26 @@
+package project.moms.assistant.presentation.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import project.moms.assistant.databinding.SleepConfiguratorFragmentBinding
+
+class SleepConfiguratorFragment : BottomSheetDialogFragment() {
+    private var _binding : SleepConfiguratorFragmentBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = SleepConfiguratorFragmentBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
