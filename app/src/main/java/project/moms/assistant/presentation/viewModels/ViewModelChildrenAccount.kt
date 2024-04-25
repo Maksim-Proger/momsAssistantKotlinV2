f2b200590346b98040f184ee8812ab4c88aedd48
Maksim <i@mpozdnjakov.ru>
2024-04-25 16:35:14 +0300

    39th commit

--- app/src/main/java/project/moms/assistant/presentation/viewModels/ViewModelChildrenAccount.kt
+++ app/src/main/java/project/moms/assistant/presentation/viewModels/ViewModelChildrenAccount.kt
@@ -1,6 +1,7 @@
 package project.moms.assistant.presentation.viewModels
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -28,6 +29,16 @@ class ViewModelChildrenAccount(private val sharedPreferences: SharedPreferences)
             val birthDate = LocalDate.parse(sharedPreferences.getDate())
             val currentDate = LocalDate.now()
             _weeks.value = ChronoUnit.WEEKS.between(birthDate, currentDate).toString()
+        }
+    }
+
+    class Factory(private val sharedPreferences: SharedPreferences) : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(ViewModelChildrenAccount::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return ViewModelChildrenAccount(sharedPreferences) as T
+            }
+            throw IllegalArgumentException("Unknown ViewModel class")
         }
     }
 
